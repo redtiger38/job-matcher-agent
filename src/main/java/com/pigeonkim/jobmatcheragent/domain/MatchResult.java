4ef8f37a354a845d3421ee2c261759802ed11ada
redtiger2012 <rlatmdrl86@gmail.com>
2026-06-02 11:02:37 +0900

    feat: MatchResult 엔티티 추가

---
+++ src/main/java/com/pigeonkim/jobmatcheragent/domain/MatchResult.java
@@ -0,0 +1,37 @@
+package com.pigeonkim.jobmatcheragent.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "match_result")
+@Getter
+@Setter
+@NoArgsConstructor
+public class MatchResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userProfileId;
+
+    private  Long jobPostingId;
+
+    @Column(columnDefinition = "TEXT")
+    private  String matchedKeywords;
+
+    @Column(columnDefinition = "TEXT")
+    private  String requirementAnalysis;
+
+    private  Integer score;
+
+    private String summary;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
